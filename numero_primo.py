3933435fdf1c1126938607c8d9afeb2a262870e2
chuda <chudacontreras@gmail.com>
2020-01-16 13:24:50 -0400

    agregado el script de nuemeros primos

---
+++ numero_primo.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+def is_prime(number):
+    if number < 2:
+        return False
+    elif number == 2:
+        return True
+    elif number > 2 and number % 2 == 0:
+        return False
+    else:
+        for i in range(3, number):
+            if number % i == 0:
+                return False
+            else:
+                return True
+
+def run():
+    #pass
+    number = int(raw_input('Escribe un numero:  '))
+
+    result = is_prime(number)
+
+    if result is  True:
+        print ('Tu Numero es primo')
+    else:
+        print(' tu nuemro NO es primo')
+
+
+
+if __name__ == "__main__":
+    run()
