d6ea38ab2c13d399f14f373c48a3d9f87e0b84a0
chuda <chudacontreras@gmail.com>
2020-02-04 15:52:14 -0400

    jugindo con el ciclo y la captura de errores agregando una opcion para salir del bucle

--- basico/errores.py
+++ basico/errores.py
@@ -1,17 +1,51 @@
 # -*- coding: utf-8 -*-
 
-countries = {
+'''countries = {
     'mexico': 122,
     'colombia': 49,
     'argentina': 43,
     'chile': 18,
-    'peru': 31
-}
+    'peru': 31,
+    'venezuela': 50
+}'''
 
-while True:
+'''while True:
     country = str(raw_input('Escribe el nombre de un país: ')).lower()
 
     try:
         print('La población de {} es: {} millones'.format(country, countries[country]))
     except KeyError:
         print('No tenemos el dato de la población de {}'.format(country))
+'''
+def main():
+    countries = {
+        'mexico': 122,
+        'colombia': 49,
+        'argentina': 43,
+        'chile': 18,
+        'peru': 31,
+        'venezuela': 50
+    }
+    opc = "s"
+
+    while opc == "s":
+        country = str(raw_input('Escribe el nombre de un pais:  ')).lower()
+
+        try:
+            print('La poblacion de {} es: {} millones'.format(country, countries[country]))
+        except KeyError:
+            print('No tenemos el dato de la población de {}'.format(country))
+        finally:
+            opc = str(raw_input('Desea consultar otro pais: s/n: ')).lower()
+            
+            
+            #opc=opc.lower
+            if opc != "s":
+                print('Saliendo del programa Gracias')
+
+
+
+        
+if __name__ == '__main__':
+    main()
+
