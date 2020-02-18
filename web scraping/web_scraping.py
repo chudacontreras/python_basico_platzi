963f0522e613d3929e4e3f22b597fcdddfd51f07
chuda <chudacontreras@gmail.com>
2020-02-17 21:22:23 -0400

    agregando scraping

--- web scraping/web_scraping.py
+++ web scraping/web_scraping.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 import urllib
+from urllib.request import urlretrieve
 
 
 
 def run():
-    for i in range(1, 1501):
+    for i in range(1, 10):
         response = requests.get('https://xkcd.com/{}'.format(i))
         soup = BeautifulSoup(response.content, 'html.parser')
         image_container = soup.find(id='comic')
@@ -13,7 +14,8 @@ def run():
         image_url = image_container.find('img')['src']
         image_name = image_url.split('/')[-1]
         print('Descargando la imagen {}'.format(image_name))
-        urllib.urlretrieve('https:{}'.format(image_url), image_name)
+        #urllib.urlretrieve('https:{}'.format(image_url), image_name)
+        urllib.request.urlretrieve('https:{}'.format(image_url), image_name)
 
 
 if __name__ == '__main__':
